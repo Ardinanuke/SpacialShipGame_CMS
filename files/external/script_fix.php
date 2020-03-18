4b5a6320f6264658e327779d0350b1c92b059cd7
luislortega <a18016323@alumnos.uady.mx>
2020-03-18 05:47:24 -0600

    Ultimo ajuste de script

--- files/external/script_fix.php
+++ files/external/script_fix.php
@@ -12,7 +12,7 @@ while($row = mysqli_fetch_array($result)){
     if($id != 1883){
         if($uridium > 1500000){
             //{"uridium":65984,"credits":1592394,"honor":38639,"experience":3996385,"jackpot":0}
-            $dataUser = '{"uridium":1500000,"credits":'.json_decode($row['data'])->credits.',"honor":'.json_decode($row['data'])->honor.',"experience":'.json_decode($row['data'])->experience.',"jackpot":'.json_decode($row['data'])->jackpot.'}';
+            $dataUser = '{"uridium":1500000,"credits":100000000,"honor":'.json_decode($row['data'])->honor.',"experience":'.json_decode($row['data'])->experience.',"jackpot":'.json_decode($row['data'])->jackpot.'}';
             $mysqli->query("UPDATE player_accounts SET data='".$dataUser."' WHERE userId=".$id);
         }
     }
