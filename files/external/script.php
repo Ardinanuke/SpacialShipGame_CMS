a720547ea000cd8da6acd6851561805771c75545
TexterDo1 <a7heerncr@gmail.com>
2020-04-04 09:29:14 -0600

    Nuevo sistema de naves y diseños agregados

---
+++ files/external/script.php
@@ -0,0 +1,15 @@
+<?php 
+require_once(INCLUDES . 'header.php'); 
+
+$mysqli = Database::GetInstance();
+
+$drones = '[{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]},{"items":[],"designs":[]}]';
+
+$mysqli->query("UPDATE player_equipment SET config1_drones ='".$drones."' , config2_drones = '".$drones."' ");
+
+$mysqli->commit();
+
+$mysqli->close();
+
+echo'<h1 style="color: white;"> Script executed.</h1>';
+?>
